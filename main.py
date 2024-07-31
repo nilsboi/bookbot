b6f0e42f06d42349ad67e99b3184d40eb6e79dd2
nilsboi <88249124+nilsboi@users.noreply.github.com>
2024-07-31 21:37:06 +0200

    Some changes on main

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello world")
+def main():
+  with open("books/frankenstein.txt") as f:
+    file_contents = f.read()
+    count(file_contents)
+    count_chars(file_contents)
+
+
+def count(txt):
+  words = txt.split()
+  print(len(words))
+  
+def count_chars(txt):
+  dic_count = {}
+  for char in txt:
+    char = char.lower()
+    if char not in dic_count:
+        dic_count[char] = 1
+    else:
+        dic_count[char] += 1
+  print(dic_count)
+  
+main()
